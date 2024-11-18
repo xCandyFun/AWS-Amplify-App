9d4727fcbdfe119b1cbd4e796830e36c110100bd
Simon T. <simtho0624@gmail.com>
2024-11-18 15:06:33 +0100

    test2

--- src/TheRouter.ts
+++ src/TheRouter.ts
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
-import Home from './components/views/StartView.vue';
 import ShowGames from './components/views/ShowGames.vue';
 import AddGame from './components/views/AddGame.vue';  // Assuming this component exists
+import StartView from './components/views/StartView.vue';
 
 // Define the routes and their components
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
-        component: Home,
+        component: StartView,
     },
     {
         path: '/ShowGames',
