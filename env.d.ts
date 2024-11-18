34b9012c8c26bf63957e8262dac876371fa317df
Simon T. <simtho0624@gmail.com>
2024-11-18 14:38:26 +0100

    Add aws polly to the website

--- env.d.ts
+++ env.d.ts
@@ -3,6 +3,10 @@
 interface ImportMetaEnv {
     readonly VITE_APP_STEP_FUNCTION_URL: string;
     readonly VITE_APP_URL_API: string;
+    readonly VITE_APP_USERPOOL_ID: string;
+    readonly VITE_APP_USERPOOL_ID_Region: string;
+    readonly VITE_APP_USERPOOL_ID_2: string;
+    readonly VITE_APP_USERPOOL_ID_Region_2: string;
     // Add other VITE_APP_ variables here
   }
   
--- src/components/views/HomeView.vue
+++
@@ -1,4 +0,0 @@
-<!-- I need this file or tsconfig.app.json don't work without it -->
-<template>
-
-</template>
